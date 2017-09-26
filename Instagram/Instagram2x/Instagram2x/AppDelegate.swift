5267a9cfdf6799b1ae57a155fd18a643d573c42e
Héctor Cuevas Morfín <poncho_nov@hotmail.com>
2017-09-26 17:02:35 -0500

    navigation controller added and tableview added

--- Instagram/Instagram2x/Instagram2x/AppDelegate.swift
+++ Instagram/Instagram2x/Instagram2x/AppDelegate.swift
@@ -17,8 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        let controller = ViewController()
-        window?.rootViewController = controller
+        let navigation = UINavigationController()
+        navigation.show(ViewController(), sender: nil)
+        window?.rootViewController = navigation
         window?.makeKeyAndVisible()
         return true
     }
---
+++ Instagram/Instagram2x/Instagram2x/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Instagram2x
+//
+//  Created by Héctor Cuevas Morfín on 9/26/17.
+//  Copyright © 2017 CloudApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    lazy var tableView: UITableView = {
+        let t = UITableView(frame: .zero)
+        t.translatesAutoresizingMaskIntoConstraints = false
+        t.delegate = self
+        t.dataSource = self
+        return t
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setUpView()  {
+        view.backgroundColor = .white
+        title = "Instagram"
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+            ])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+        
+    }
+}
